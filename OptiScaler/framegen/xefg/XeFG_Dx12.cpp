cb6354fdeb5efccb57bfca701df029413d424324
cdozdil <cdozdil@gmail.com>
2026-05-31 23:09:20 +0300

    XeFG interpolation count fix v2

--- OptiScaler/framegen/xefg/XeFG_Dx12.cpp
+++ OptiScaler/framegen/xefg/XeFG_Dx12.cpp
@@ -354,8 +354,8 @@ bool XeFG_Dx12::CreateSwapchain(IDXGIFactory* factory, ID3D12CommandQueue* cmdQu
     if (_framesToInterpolate > intTarget)
         _framesToInterpolate = intTarget;
 
-    if (!Config::Instance()->FGXeFGInterpolationCount.has_value())
-        Config::Instance()->FGXeFGInterpolationCount.set_volatile_value(intTarget);
+    //if (!Config::Instance()->FGXeFGInterpolationCount.has_value())
+    //    Config::Instance()->FGXeFGInterpolationCount.set_volatile_value(intTarget);
 
     params.maxInterpolatedFrames = intTarget;
 
@@ -513,8 +513,8 @@ bool XeFG_Dx12::CreateSwapchain1(IDXGIFactory* factory, ID3D12CommandQueue* cmdQ
     if (_framesToInterpolate > intTarget)
         _framesToInterpolate = intTarget;
 
-    if (!Config::Instance()->FGXeFGInterpolationCount.has_value())
-        Config::Instance()->FGXeFGInterpolationCount.set_volatile_value(intTarget);
+    // if (!Config::Instance()->FGXeFGInterpolationCount.has_value())
+    //     Config::Instance()->FGXeFGInterpolationCount.set_volatile_value(intTarget);
 
     params.maxInterpolatedFrames = intTarget;
 
